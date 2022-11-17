76437cceb7d0af61c95a4951628605e3e24d52ee
Danielle Tate <danielletate@Danielles-MacBook-Air.local>
2022-11-17 12:01:02 -0700

    finish ui

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = 'fe17b0d21481d6ec741f8e32982fe5bb';
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
